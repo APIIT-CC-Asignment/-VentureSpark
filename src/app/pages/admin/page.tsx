0e2102ab5a853e2aced23e0bd4de57f72ed91e86
Abdul Hakeem <cb013168@students.apiit.lk>
2025-05-27 23:42:36 +0530

    update user

--- src/app/pages/admin/page.tsx
+++ src/app/pages/admin/page.tsx
@@ -851,10 +851,7 @@ export default function AdminDashboard() {
                               Active
                             </span>
                           </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                            <button className="text-blue-600 hover:text-blue-900 mr-3">Edit</button>
-                            <button className="text-red-600 hover:text-red-900">Delete</button>
-                          </td>
+                          
                         </tr>
                       ))}
                   </tbody>
